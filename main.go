c6c1dd5a5469af10c368a23e22621cacd37cae3b
Samuel <samuel@kakao.com>
2015-03-29 01:57:43 +0900

    init..

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/ddliu/go-httpclient"
+
+func main() {
+	httpclient.Defaults(httpclient.Map{
+		httpclient.OPT_USERAGENT: "googlebot",
+		"Accept-Language":        "kr",
+	})
+
+	res, err := httpclient.Get("http://google.com/search", map[string]string{
+		"q": "news",
+	})
+
+	println(res.StatusCode, err)
+}
+
